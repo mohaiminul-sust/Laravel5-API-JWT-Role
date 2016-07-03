28a0f700f9b0c88073c557fa0eb5e66ac55c6efa
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-07-04 02:32:17 +0600

    asses user seed

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -11,6 +11,24 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        // $this->call(UserTableSeeder::class);
+        DB::table('users')->delete();
+
+        $users = array(
+            ['name' => 'Luke Skywalker', 'email' => 'luke@gmail.com', 'password' => Hash::make('luke')],
+            ['name' => 'Muremaru Ochikawaga', 'email' => 'japani_halar_put@gmail.com', 'password' => Hash::make('nippon')],
+            ['name' => 'Gojira', 'email' => 'seriously@gmail.com', 'password' => Hash::make('g')],
+            ['name' => 'Mohaiminul Islam', 'email' => 'mohaiminul.sust@gmail.com', 'password' => Hash::make('a')],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user)
+        {
+            User::create($user);
+        }
+
+        Model::reguard();
     }
 }
