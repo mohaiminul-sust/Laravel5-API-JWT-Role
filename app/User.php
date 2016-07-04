8687c125aef38fac5d920040dc346fe5b3278dce
mohaiminul-sust <mohaiminul.sust@csesociety.org>
2016-07-04 12:50:31 +0600

    added entrust based models and configured user model as necessary

--- app/User.php
+++ app/User.php
@@ -2,25 +2,10 @@
 
 namespace App;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
+use Zizaco\Entrust\Traits\EntrustUserTrait;
 
-class User extends Authenticatable
-{
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array
-     */
-    protected $fillable = [
-        'name', 'email', 'password',
-    ];
-
-    /**
-     * The attributes that should be hidden for arrays.
-     *
-     * @var array
-     */
-    protected $hidden = [
-        'password', 'remember_token',
-    ];
+class User extends Eloquent{
+    
+    use Authenticatable, CanResetPassword, EntrustUserTrait;
+    ...
 }
